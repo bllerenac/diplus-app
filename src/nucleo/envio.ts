0f2e1e4a228aa8714136fe2183f2d18737ac6c2e
BryanLlerena <bllerenac@uni.pe>
2026-09-21 14:48:45 -0500

    fix: update default MQTT topic to miskimayo/{{unit_id}}/telemetry

--- src/nucleo/envio.ts
+++ src/nucleo/envio.ts
@@ -150,7 +150,7 @@ export const ENVIO_POR_DEFECTO: AjustesEnvio = {
     puerto: 1883,
     usuario: 'test',
     contrasena: 'test1234',
-    topic: '/miskimayo/diplus/{{unit_id}}',
+    topic: 'miskimayo/{{unit_id}}/telemetry',
     cadaSeg: 5,
   },
   equipo: 'SC-03',
